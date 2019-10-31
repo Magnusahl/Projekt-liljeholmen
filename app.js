3d6c9ac07a338727650bd73a9b62e07b1e58ed27
Magnus Ahlqvist <51696488+Magnusahl@users.noreply.github.com>
2019-10-31 14:15:11 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function getLocation()
   {
   if (navigator.geolocation)
     {
-    navigator.geolocation.getCurrentPosition(showPosition,showError);
+    navigator.geolocation.getCurrentPosition(showPosition);
     }
   else{x.innerHTML="Geolocation is not supported by this browser.";}
   }
@@ -25,25 +25,4 @@ function showPosition(position)
   };
   var map=new google.maps.Map(document.getElementById("mapholder"),myOptions);
   var marker=new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
-  }
-
-  function showError(error)
-  {
-  switch(error.code) 
-    {
-    case error.PERMISSION_DENIED:
-      x.innerHTML="User denied the request for Geolocation."
-      break;
-    case error.POSITION_UNAVAILABLE:
-      x.innerHTML="Location information is unavailable."
-      break;
-    case error.TIMEOUT:
-      x.innerHTML="The request to get user location timed out."
-      break;
-    case error.UNKNOWN_ERROR:
-      x.innerHTML="An unknown error occurred."
-      break;
-    }
-  }
-
-
+}
